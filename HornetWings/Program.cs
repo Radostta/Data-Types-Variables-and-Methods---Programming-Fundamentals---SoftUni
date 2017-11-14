d675005ef11447aa5899108d12d3e0d7efdb47ca
Radostina Cvetkova <radost.tsvetkova@gmail.com>
2017-11-14 11:25:44 +0200

    Adding the rest of the problems in exercise

---
+++ HornetWings/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HornetWings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long wingFlaps = long.Parse(Console.ReadLine());
+            double distancePer1000Flaps = double.Parse(Console.ReadLine()); //in meters
+            long endurance = long.Parse(Console.ReadLine()); //in flaps
+
+            var distance = (wingFlaps / 1000) * distancePer1000Flaps;          
+            var seconds = (wingFlaps / endurance) * 5;
+
+            seconds += wingFlaps / 100;
+
+            Console.WriteLine($"{distance:f2} m.");
+            Console.WriteLine($"{seconds} s.");           
+
+        }
+    }
+}
---
+++ WormTest/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WormTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int wormLength = int.Parse(Console.ReadLine()); //in meters
+            double wormWidth = double.Parse(Console.ReadLine()); //in cm
+
+            wormLength *= 100; //in cm
+            double result = 0;
+
+            if (wormWidth == 0 || wormLength % wormWidth == 0)
+            {
+                result = (double)wormLength * wormWidth;
+                Console.WriteLine($"{result:f2}");
+            }
+            else
+            {
+                result = ((double)wormLength / wormWidth) * 100;
+                Console.WriteLine($"{result:f2}%");
+            }
+
+           
+
+        }
+    }
+}
